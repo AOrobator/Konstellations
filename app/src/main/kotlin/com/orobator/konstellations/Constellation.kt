18fac1d152e52fc965c7d12281be474c6ed848e4
AndrewOrobator <Orobator.Andrew@gmail.com>
2016-11-12 17:28:13 -0500

    Added missing constellations Escaped quotation marks Added up button in Detail Activity

--- app/src/main/kotlin/com/orobator/konstellations/Constellation.kt
+++ app/src/main/kotlin/com/orobator/konstellations/Constellation.kt
@@ -3,6 +3,7 @@ package com.orobator.konstellations
 enum class Constellation(val constellationName: String, desc: String) {
   AQUARIUS(R.string.aquarius.getString(), R.string.aquarius_desc.getString()),
   AQUILA(R.string.aquila.getString(), R.string.aquila_desc.getString()),
+  ARIES(R.string.aries.getString(), R.string.aries_desc.getString()),
   AURIGA(R.string.auriga.getString(), R.string.auriga_desc.getString()),
   BOOTES(R.string.bootes.getString(), R.string.bootes_desc.getString()),
   CANCER(R.string.cancer.getString(), R.string.cancer_desc.getString()),
@@ -16,11 +17,12 @@ enum class Constellation(val constellationName: String, desc: String) {
   LYRA(R.string.lyra.getString(), R.string.lyra_desc.getString()),
   ORION(R.string.orion.getString(), R.string.orion_desc.getString()),
   PEGASUS(R.string.pegasus.getString(), R.string.pegasus_desc.getString()),
+  PERSEUS(R.string.perseus.getString(), R.string.perseus_desc.getString()),
   PISCES(R.string.pisces.getString(), R.string.pisces_desc.getString()),
   SAGITTARIUS(R.string.sagittarius.getString(), R.string.sagittarius_desc.getString()),
   SCORPIUS(R.string.scorpius.getString(), R.string.scorpius_desc.getString()),
   TAURUS(R.string.taurus.getString(), R.string.taurus_desc.getString()),
-  URSA_MAJOR(R.string.ursa_major.getString(), R.string.ursa_major_dsec.getString()),
+  URSA_MAJOR(R.string.ursa_major.getString(), R.string.ursa_major_desc.getString()),
   VIRGO(R.string.virgo.getString(), R.string.virgo_desc.getString());
 
   val description = desc + R.string.content_attribution.getString()
