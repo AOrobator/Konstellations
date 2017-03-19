b1190ed9213fff509e5ecb40e40327cd8f8d50f7
AndrewOrobator <Orobator.Andrew@gmail.com>
2017-03-19 19:48:53 -0400

    Upgraded kotlin and gradle

--- app/src/main/kotlin/com/orobator/konstellations/ShortcutHelper.kt
+++ app/src/main/kotlin/com/orobator/konstellations/ShortcutHelper.kt
@@ -1,27 +1,23 @@
 package com.orobator.konstellations
 
 import android.annotation.TargetApi
-import android.content.pm.ShortcutInfo.Builder
+import android.content.pm.ShortcutInfo
 import android.content.pm.ShortcutManager
 import android.graphics.drawable.Icon
-import android.os.Build.VERSION.SDK_INT
+import android.os.Build
 import android.os.Build.VERSION_CODES.N_MR1
 import android.preference.PreferenceManager
 import com.orobator.konstellations.KonstellationsApplication.Companion.APP_CONTEXT
-import kotlin.comparisons.compareBy
 
-@TargetApi(N_MR1)
-fun trackShortcutUsed(shortcutManager: ShortcutManager, constellation: Constellation) {
-  shortcutManager.reportShortcutUsed(constellation.name)
-
-  val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(APP_CONTEXT)
-  val seenCount = sharedPrefs.getInt(constellation.name, 0)
-  sharedPrefs
-      .edit()
-      .putInt(constellation.name, seenCount + 1)
-      .apply()
-
-  updateShortcuts(shortcutManager)
+/**
+ * Helper method to pass in usages of the shortcut api without having to do an
+ * explicit version check every time
+ */
+inline fun shortcutAction(action: (ShortcutManager) -> Unit): Unit {
+  if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) {
+    val shortcutManager = APP_CONTEXT.getSystemService(ShortcutManager::class.java)
+    action(shortcutManager)
+  }
 }
 
 /**
@@ -43,7 +39,7 @@ fun updateShortcuts(shortcutManager: ShortcutManager) {
           .values()
           .sortedWith(compareBy { -getConstellationVisitedCount(it) })
           .map {
-            Builder(APP_CONTEXT, it.name)
+            ShortcutInfo.Builder(APP_CONTEXT, it.name)
                 .setShortLabel(it.shortName)
                 .setLongLabel(it.longName)
                 .setIcon(Icon.createWithResource(APP_CONTEXT, R.drawable.shortcut_icon))
@@ -59,13 +55,16 @@ fun updateShortcuts(shortcutManager: ShortcutManager) {
           }.subList(0, 3)
 }
 
-/**
- * Helper method to pass in usages of the shortcut api without having to do an
- * explicit version check every time
- */
-inline fun shortcutAction(action: (ShortcutManager) -> Unit): Unit {
-  if (SDK_INT >= N_MR1) {
-    val shortcutManager = APP_CONTEXT.getSystemService(ShortcutManager::class.java)
-    action(shortcutManager)
-  }
+@TargetApi(N_MR1)
+fun trackShortcutUsed(shortcutManager: ShortcutManager, constellation: Constellation) {
+  shortcutManager.reportShortcutUsed(constellation.name)
+
+  val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(APP_CONTEXT)
+  val seenCount = sharedPrefs.getInt(constellation.name, 0)
+  sharedPrefs
+      .edit()
+      .putInt(constellation.name, seenCount + 1)
+      .apply()
+
+  updateShortcuts(shortcutManager)
 }
