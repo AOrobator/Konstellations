e29e2a1d5d8a02a58354e36a722b3ed4285122f0
AndrewOrobator <Orobator.Andrew@gmail.com>
2017-07-09 10:41:00 -0400

    Created shortcut customization activity

---
+++ app/src/main/java/com/orobator/konstellations/ShortcutCustomizationActivity.java
@@ -0,0 +1,12 @@
+package com.orobator.konstellations;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ShortcutCustomizationActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_shortcut_customization);
+  }
+}
